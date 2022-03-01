bd1108ff1535134b999a0da7c726d13788d057fa
Samuel Kostadinov <SamuelKostadinovGit@gmail.com>
2022-03-01 20:12:36 +0100

    Inizio solver non funzionante

--- script.js
+++ script.js
@@ -41,25 +41,297 @@ document.addEventListener("DOMContentLoaded", (event)=>{
 	fillCells()
 })
 
+document.addEventListener('keydown', (event)=>{
+
+	if(event.keyCode == '37'){
+		//arrow left
+		if(currentCell > 0){
+
+			currentCell --
+			adjournCurrentCell(currentCell)
+		}
+	}
+	if(event.keyCode == '38'){
+		//arrow up
+		if(currentCell > 8){
+
+			currentCell -= 9
+			adjournCurrentCell(currentCell)
+		}
+	}
+	if(event.keyCode == '39'){
+		//arrow right
+		if(currentCell < 80){
+
+			currentCell ++
+			adjournCurrentCell(currentCell)
+		}
+	}
+	if(event.keyCode == '40'){
+		//arrow down
+		if(currentCell < 72){
+
+			currentCell += 9
+			adjournCurrentCell(currentCell)
+		}
+	}
+	if(event.keyCode == '49' || event.keyCode == '97'){
+		//1
+		insert(1)
+	}
+	if(event.keyCode == '50' || event.keyCode == '98'){
+		//2
+		insert(2)
+	}
+	if(event.keyCode == '51' || event.keyCode == '99'){
+		//3
+		insert(3)
+	}
+	if(event.keyCode == '52' || event.keyCode == '100'){
+		//4
+		insert(4)
+	}
+	if(event.keyCode == '53' || event.keyCode == '101'){
+		//5
+		insert(5)
+	}
+	if(event.keyCode == '54' || event.keyCode == '102'){
+		//6
+		insert(6)
+	}
+	if(event.keyCode == '55' || event.keyCode == '103'){
+		//7
+		insert(7)
+	}
+	if(event.keyCode == '56' || event.keyCode == '104'){
+		//8
+		insert(8)
+	}
+	if(event.keyCode == '57' || event.keyCode == '105'){
+		//9
+		insert(9)
+	}
+	if(event.keyCode == '46' || event.keyCode == "8"){
+		//canc or backspace
+		deleteCell()
+	}
+})
+
 function fillCells(array=[]){
 
-	//TEMP IMPLEMENTATION FOR TEST PURPOSES
-	//let arr = [1,2,3,1,2,3,1,2,3]
-	//let arr2 = [4,5,6,4,5,6,4,5,6]
-	//let arr3 = [7,8,9,7,8,9,7,8,9]
-
-	//let total = [arr, arr2, arr3, arr, arr2, arr3, arr, arr2, arr3]
-
-	//console.log(total)
-
-	/*
+	//Temporary implementation
+	let total = [
+					["", "", "", "5", "1", "", "", "3", ""],
+					["3", "9", "1", "6", "7", "", "5", "", "2"],
+					["6", "2", "", "8", "9", "", "4", "1", "7"],
+					["", "3", "2", "", "", "", "", "6", "4"],
+					["", "", "", "", "6", "", "2", "", "8"],
+					["8", "", "", "", "", "1", "", "", ""],
+					["2", "5", "", "", "3", "7", "", "", ""],
+					["", "1", "8", "", "", "6", "7", "", ""],
+					["", "", "", "", "2", "", "", "4", ""]
+				]
+	
 	for(let i = 0; i < 9; i++){
 		for(let j = 0; j < 9; j++){
 
 			document.getElementById(i * 9 + j).innerHTML = total[i][j]
 		}
 	}
-	*/
+}
+
+function removeByValue(arr, value){
+
+	if(!Array.isArray(arr)){
+
+		return arr
+	}
+
+	let index = arr.indexOf(value)
+	
+	if(index != -1){
+
+		arr.splice(index, 1)
+	}
+
+	return arr
+}
+
+function removeFromRow(domains, value, index){
+
+	if(index > 80 || index < 0){
+
+		alert("something went wrong")
+		return domains
+	}
+
+	let row = Math.floor(index / 9)
+	
+	for(let i = 0; i < 9; i++){
+
+		//console.log(domains[i])
+		let auxIndex = row * 9 + i
+		if(Array.isArray(domains[auxIndex]) && (auxIndex != index)){
+
+			domains[auxIndex] = removeByValue(domains[i], value)
+		}
+	}
+
+	return domains
+}
+
+function removeFromCol(domains, value, index){
+
+	if(index > 80 || index < 0){
+
+		alert("something went wrong")
+		return domains
+	}
+
+	let col = index % 9
+	
+	for(let i = 0; i < 9; i++){
+
+		let auxIndex = i * 9 + col
+		if(Array.isArray(domains[auxIndex]) && (auxIndex != index)){
+
+			domains[auxIndex] = removeByValue(domains[i], value)
+		}
+	}
+
+	return domains
+}
+function removeFromSquare(domains, value, index){
+
+	return domains
+}
+
+function restrictDomains(domains){
+}
+
+function allSingleDomains(domains){
+
+	for(let i = 0; i < 81; i++){
+		if(!Array.isArray(domains[i])){
+
+			return false
+		}
+	}
+}
+
+function solve(){
+
+	alert("Still not working")
+	domains = [
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9],
+					[1,2,3,4,5,6,7,8,9]
+			  ]
+
+	let values = []
+	for(let i = 0; i < 81; i++){
+
+		let n = document.getElementById(i).innerHTML;
+		console.log(n)
+		if(n != ""){
+			console.log("id " + i)
+			domains[i] = parseInt(n)
+		}		
+	}
+
+
+	//console.log(values)
+	//console.log(domains)
+	let maxN = 10
+	removeFromRow(domains, 5, 3)
+	removeFromCol(domains, 5, 3)
+
+	//console.log(domains)
+
+	/*while(!allSingleDomains(domains) && (maxN > 0)){
+
+		restrictDomains(domains)
+		maxN--
+	}*/
+
 }
 
 function createSudoku(){}
@@ -183,29 +455,7 @@ function adjournCurrentCell(id){
 	for(let i = 0; i < 81; i++){
 
 		document.getElementById(i).style.backgroundColor = "#fff"
-	}	
-
-	/*console.log("currentCell fatto")
-	document.getElementById(currentCell).style.backgroundColor = "#fff"
-
-	for(let i = 0; i < 9; i++){
-
-
-		document.getElementById(row * 9 + i).style.backgroundColor = "#fff"
-		document.getElementById(i * 9 + col).style.backgroundColor = "#fff"
 	}
-
-	console.log("Righe e colonne")
-	
-	for(let i = 0; i < 3; i++){
-		for(let j = 0; j < 3; j++){
-
-			let id = (startingRow * 9) + (i * 9) + (startingCol) + j
-			document.getElementById(id).style.backgroundColor = "#fff"
-		}
-	}	
-
-	console.log("Quadrati")*/
 
 	currentCell = id;
 	row = Math.floor(currentCell / 9)
@@ -277,75 +527,3 @@ function resetAllCells(){
 	}
 }
 
-document.addEventListener('keydown', (event)=>{
-
-	if(event.keyCode == '37'){
-		//arrow left
-		if(currentCell > 0){
-
-			currentCell --
-			adjournCurrentCell(currentCell)
-		}
-	}
-	if(event.keyCode == '38'){
-		//arrow up
-		if(currentCell > 8){
-
-			currentCell -= 9
-			adjournCurrentCell(currentCell)
-		}
-	}
-	if(event.keyCode == '39'){
-		//arrow right
-		if(currentCell < 80){
-
-			currentCell ++
-			adjournCurrentCell(currentCell)
-		}
-	}
-	if(event.keyCode == '40'){
-		//arrow down
-		if(currentCell < 72){
-
-			currentCell += 9
-			adjournCurrentCell(currentCell)
-		}
-	}
-	if(event.keyCode == '49' || event.keyCode == '97'){
-		//1
-		insert(1)
-	}
-	if(event.keyCode == '50' || event.keyCode == '98'){
-		//2
-		insert(2)
-	}
-	if(event.keyCode == '51' || event.keyCode == '99'){
-		//2
-		insert(3)
-	}
-	if(event.keyCode == '52' || event.keyCode == '100'){
-		//2
-		insert(4)
-	}
-	if(event.keyCode == '53' || event.keyCode == '101'){
-		//2
-		insert(5)
-	}
-	if(event.keyCode == '54' || event.keyCode == '102'){
-		//2
-		insert(6)
-	}
-	if(event.keyCode == '55' || event.keyCode == '103'){
-		//2
-		insert(7)
-	}
-	if(event.keyCode == '56' || event.keyCode == '104'){
-		//2
-		insert(8)
-	}
-	if(event.keyCode == '57' || event.keyCode == '105'){
-		//2
-		insert(9)
-	}
-
-})
